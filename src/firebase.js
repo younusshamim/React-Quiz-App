a6d88e0fd85c06af18f957ac87f0246c3646ca1f
Younus <younusshamim.bd@gmail.com>
2022-05-08 22:52:06 +0600

    env.local file remove (remove the firebase security)

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 
 const app = initializeApp({
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_ID,
-  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  apiKey: "AIzaSyBDQZBwEUA_HjPFtLq668bNHBFQ-XwxpNU",
+  authDomain: "react-quiz-dev-ecfd4.firebaseapp.com",
+  databaseURL: "https://react-quiz-dev-ecfd4-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "react-quiz-dev-ecfd4",
+  storageBucket: "react-quiz-dev-ecfd4.appspot.com",
+  messagingSenderId: "804789001529",
+  appId: "1:804789001529:web:c49fee57a9924291131ce8"
 });
 
 export default app;
